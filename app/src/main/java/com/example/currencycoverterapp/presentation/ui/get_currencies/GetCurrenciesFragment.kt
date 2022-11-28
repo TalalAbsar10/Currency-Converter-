315b4b1ab621e9dea0c343ac0ee993a3f530a521
Talal <talal.absar619@gmail.com>
2022-11-29 01:35:28 +0400

    Final code, removed button to call a service and turned night mode off

--- app/src/main/java/com/example/currencycoverterapp/presentation/ui/get_currencies/GetCurrenciesFragment.kt
+++ app/src/main/java/com/example/currencycoverterapp/presentation/ui/get_currencies/GetCurrenciesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -47,9 +48,10 @@ class GetCurrenciesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnCallService.setOnClickListener {
-            callAPI(BASE_CURRENCY)
-        }
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        callAPI(BASE_CURRENCY)
+
         binding.btnCalculate.setOnClickListener {
             if (binding.etAmount.text.toString() != "") {
                 selectedCurrencyConversionRate = formatToToDigits(
